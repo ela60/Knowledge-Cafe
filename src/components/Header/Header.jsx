fcc396f94adec9cdc27faf5afbe2689708b69c9b
Ambia-Ela <ambiaela7275@gmail.com>
2024-10-22 08:17:52 +0600

    loaded json data

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+          <h1 className='text-6xl bg-slate-500'>Knowledge Cafe</h1>  
+        </div>
+    );
+};
+
+export default Header;
